db7cb93c2f36c90792b0dbbddb31d9fda0783af9
Zain Ahmed <154746358+Zain4YESA@users.noreply.github.com>
2024-07-18 18:31:02 +0500

    Update Message Protector.py

--- Message Protector.py
+++ Message Protector.py
@@ -1,21 +1,20 @@
 from cryptography.fernet import Fernet
 
-# Generate a key
+
 key = Fernet.generate_key()
 cipher_suite = Fernet(key)
-message = input("Enter your message: ")
-choice = input("If you want to encode your message, type 1 or to decode enter 2: ")
 
-if choice == '1':
-    # Encrypt a message
-    cipher_text = cipher_suite.encrypt(message.encode())  # Encode the message to bytes
-    print(f"Encrypted message: {cipher_text}")
-elif choice == '2':
-    # Ensure there's a cipher text to decrypt
-    try:
-        plain_text = cipher_suite.decrypt(cipher_text).decode()  # Decode to string
-        print(f"Decrypted message: {plain_text}")
-    except NameError:
-        print("Error: No encrypted message found. Please encrypt a message first.")
+message = input("Enter your message: ")
+
+cipher_text = cipher_suite.encrypt(message.encode())  # Encode the message to bytes
+print(f"Encrypted message: {cipher_text}")
+
+choice = input("Do you want to decrypt it? Yes or No: ")
+
+if choice.lower() == "yes":
+    
+    plain_text = cipher_suite.decrypt(cipher_text).decode()  # Decode to string
+    print(f"Decrypted message: {plain_text}")
 else:
-    print("Invalid choice. Please enter 1 to encode or 2 to decode.")
+    print("No decryption!")
+
